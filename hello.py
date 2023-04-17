40ffc9aa4297ad40f56e715b38acd8b66cd58402
Y.Nanthachai <thecoachingonline@gmail.com>
2023-04-17 05:57:16 +0000

    01100101111110010011

--- hello.py
+++ hello.py
@@ -1,14 +1,18 @@
 import cirq
 
-# Create a circuit with one qubit
-circuit = cirq.Circuit()
-qubit = cirq.LineQubit(0)
-circuit.append(cirq.X(qubit))
-circuit.append(cirq.measure(qubit, key='result'))
+# Pick a qubit.
+qubit = cirq.GridQubit(0, 0)
 
-# Simulate the circuit using the Cirq simulator
+# Create a circuit
+circuit = cirq.Circuit(
+    cirq.X(qubit)**0.5,  # Square root of NOT.
+    cirq.measure(qubit, key='m')  # Measurement.
+)
+print("Circuit:")
+print(circuit)
+
+# Simulate the circuit several times.
 simulator = cirq.Simulator()
-result = simulator.run(circuit)
-
-# Print the result of the simulation
-print(result.measurements['result'])
+result = simulator.run(circuit, repetitions=20)
+print("Results:")
+print(result)
